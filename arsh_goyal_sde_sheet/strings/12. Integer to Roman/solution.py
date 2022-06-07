5de2d4fab98d5bc681b11012f64c81e424644b74
sidheshwar-s <sidheshwarsaravanan@gmail.com>
2022-06-07 21:24:08 +0530

    :pencil2: Added solution for 12. Integer to Roman

---
+++ arsh_goyal_sde_sheet/strings/12. Integer to Roman/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        romans = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'} 
+        
+        res = ""
+        for val, roman in romans.items():
+            res += (num // val) * roman
+            num %= val
+        return res
+        
